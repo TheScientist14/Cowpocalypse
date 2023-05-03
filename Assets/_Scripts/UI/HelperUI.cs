83e9a400597fa9e13e7ad3a9c1e3d72bbb835079
superzero4 <78915984+superzero4@users.noreply.github.com>
2023-05-03 19:05:06 +0200

    Renamed and fix the mess with helps and filename

---
+++ Assets/_Scripts/UI/HelperUI.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[System.Serializable]
+public class TextUI
+{
+    [SerializeField]
+    private string _value;
+    [SerializeField]
+    private TMP_Text _text;
+
+    public string Value { get => _value; set => _value = value; }
+    void UpdateText()
+    {
+        _text.text = _value;
+    }
+}
