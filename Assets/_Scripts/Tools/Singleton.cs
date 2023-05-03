b5807684d1903d7d71cf02ca92eb9159860c4673
superzero4 <78915984+superzero4@users.noreply.github.com>
2023-05-03 15:04:00 +0200

    Add singleton script

---
+++ Assets/_Scripts/Tools/Singleton.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public static T instance
+    {
+        get
+        {
+            return _instance ??= FindAnyObjectByType<T>() ?? new GameObject(typeof(T).Name + " " + nameof(Singleton<T>)).AddComponent<T>();
+        }
+    }
+    private static T _instance;
+}
