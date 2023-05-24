9397975fec4cf27bbd774d8012957c9a0aa47028
Corentin <ccouasno@gmail.com>
2023-05-24 13:11:04 +0200

    Logic d'unlock des item data

--- Assets/_Scripts/Items/ItemTier.cs
+++ Assets/_Scripts/Items/ItemTier.cs
@@ -5,4 +5,5 @@ public class ItemTier : ScriptableObject
 {
     [field: SerializeField] public int Level { get; private set; }
     [field: SerializeField] public string Name { get; private set; }
+    [field: SerializeField] public int UnlockPrice { get; private set; }
 }
